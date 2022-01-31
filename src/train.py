3ea2a6651bfcb6c4e4e82feca93db7764e454fab
ldiao <ldiao@ldiao-23325.pods.ldiao.svc.cluster.local>
2022-01-30 17:02:09 -0800

    change metric

--- src/train.py
+++ src/train.py
@@ -30,10 +30,8 @@ def getAllCombinations( cond_):
     lst =  ['total_bytes','max_bytes', "1->2Bytes",'2->1Bytes'
                 ,'1->2Pkts','2->1Pkts','total_pkts','number_ms', 'pkt_ratio','time_spread', 'pkt sum','longest_seq'
                 ,'total_pkt_sizes', 'mean_tdelta', 'max_tdelta']# 'proto',
-    lst1 = ["max_bytes", "longest_seq", "total_bytes" ] # more later
-    lst2 = ["total_pkts", "number_ms", "byte_ratio"]
-    latency_lst = [ "byte_ratio", 'max_tdelta', 'pkt_ratio', 'time_spread_min', ]
-    loss_lst = ['total_pkts', 'pkt sum', 'total_pkt_sizes', '2->1Bytes', 'total_pkts_max', 'number_ms', 'mean_tdelta'] #  'total_bytes', '2->1Pkts', 
+    latency_lst = [ "byte_ratio", 'max_tdelta', 'pkt_ratio', 'time_spread_min', 'total_bytes', '2->1Pkts']
+    loss_lst = ['total_pkts', 'pkt sum', 'total_pkt_sizes', '2->1Bytes', 'total_pkts_max', 'number_ms', 'mean_tdelta'] 
     
     if cond_ == 1:
         lst = loss_lst
